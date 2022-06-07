641d8640e4bb3eeab64f206510806159ca2d9894
cristhian valencia <cristhian.valencia@nuevatel.com>
2022-06-07 11:04:54 -0400

    Guia para la actualizacion de Angular V13 a V14

--- src/app/pages/check-coverage/check-coverage.component.ts
+++ src/app/pages/check-coverage/check-coverage.component.ts
@@ -10,13 +10,16 @@ export class CheckCoverageComponent implements OnInit {
   zoom = 118;
   center: google.maps.LatLngLiteral = { lat : -17.393847, lng:-66.157328};
   options: google.maps.MapOptions = {
-    mapTypeId: 'hybrid',
+    //mapTypeId: 'hybrid',
     zoomControl: true,
     scrollwheel: true,
     disableDoubleClickZoom: true,
     maxZoom: 18,
     minZoom: 8,
-  }
+  };
+
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
 
   constructor() { }
 
@@ -32,6 +35,14 @@ export class CheckCoverageComponent implements OnInit {
 
   verificarCobertura(): void {
 
+  }
+
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    console.log("click en el mapa", event.latLng?.toJSON());
+    if(event.latLng != null){
+      this.markerPositions.push(event.latLng.toJSON());
+    }
   }
 
 }
