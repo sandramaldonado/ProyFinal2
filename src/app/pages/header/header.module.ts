82d1c18222971b0ecf5e723cc84859ff5c86841d
cristhian valencia <cristhian.valencia@nuevatel.com>
2022-06-07 14:22:07 -0400

    add options to menu for navigation

--- src/app/pages/header/header.module.ts
+++ src/app/pages/header/header.module.ts
@@ -5,6 +5,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from "@angular/material/menu";
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +18,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatIconModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
+    RouterModule
   ],
   exports:[HeaderComponent]
 })
