1f9c7f82d7cdb1b8d7f3498f1d5e50a080e1ea5e
Victor Antonio Zurita Borja <victor.zurita@nuevatel.com>
2022-06-22 10:48:46 -0400

    cargar cambios validation

--- src/app/shared/services/validation.service.ts
+++ src/app/shared/services/validation.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidationService {
+  apiUrl: string = 'http://10.47.17.36:3000/client';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  getClient(dni: any, phone: any): Observable<any> {
+    const addUrlDni = '?dni=';
+    const addUrlPhone = '&subscriberId=';
+    return this.httpClient.get(`${this.apiUrl}${addUrlDni}${dni}${addUrlPhone}${phone}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Handle API errors
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
