e4936f986587d253e43b7380bb29403e1c767582
Cristhian Emerson Valencia Felipez <cristhian.valencia@nuevatel.com>
2022-08-07 22:36:57 -0400

    generate build for develop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,19 @@ const vars = EnvServiceFactory();
 console.log("vars",vars)
 export const environment = {
   production: false,
- 
+
   url_api_jsonplaceholder: 'https://jsonplaceholder.typicode.com',
   url_api_simplekart:'https://simplekart.postman.com',
   url_api_simplekart_mock_server:'https://f0be739e-4263-42e4-b8a5-c00ed2e46fc6.mock.pstmn.io',
-  
+
+  AVLTokenKey :'x-access-token',
+  AVLTokenValue :'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDhhNGRlNmM1OTgwYTI5MzRjNTVmZDMiLCJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6IlNVUEVSQURNSU4iLCJpYXQiOjE1NjkzNDc5NzZ9.ac-KlxXxEijwGtI141jp6bA1_8CUFrFcdvysur8mz9Y',
+  AVLApiUrl : 'http://gis.nuevatel.com/backcustomersite/api/customersite',
+
+  SSITokenUsername : 'landing',
+  SSITokenPassword : '3X4E+U7d1m1XprRamjfaRg==',
+  SSIAuthApiUrl : 'http://omega.10.45.57.104.sslip.io/auth/login',
+
   domain: vars.domain,
   endPoint: vars.endPoint,
   urlClientInfoMicrofront: vars.urlClientInfoMicrofront,
@@ -19,5 +27,5 @@ export const environment = {
   urlVasMicrofront: vars.urlVasMicrofront,
   urlDashboardMicrofront: vars.urlDashboardMicrofront,
   applicationId: 2
- 
+
 };
