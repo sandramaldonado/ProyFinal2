9dd60f829be221a8c30507eeee88d633c9e7f949
AlexSajama <alex.sajama@salamancasolutions.com>
2022-08-16 18:38:00 -0400

    payment input data

--- src/app/modules/sales/card-payment/card-payment.component.ts
+++ src/app/modules/sales/card-payment/card-payment.component.ts
@@ -15,26 +15,13 @@ export class CardPaymentComponent implements OnInit {
         id: 'card-pay-microfront-app'
     }
     public userId: any;
-    public microFrontParamIn = {
-        theme: "light-green",
-        orderType: "SALES",
-        orderId: "1000000568",
-        channel: "LANDING",
-        entityType: "customerId",
-        entityId: "1098757",
-        language: "es",
-        termsOfService: "{'mode':'optional','url':'www.google.com'}", // mode: required | option
-        amount: "150.90",
-        currency: "150.90",
-        cart: "[{'sellerId':'NT', 'sellerDesc':'Viva', 'detail':[{'code':'HU-P20','desc':'HUAWEI P20 Pro', 'amount':120.90', 'currency':'Bs'},{'code':'HF120','desc':'Hands free', 'amount':30.00'', 'currency':'Bs'}]]",
-        recurring: "required", // required | option
-        user: {},
-        microfrontId: 'card-pay-microfront-app',
-    }
+    public microFrontParamIn: any = {}
     public paramClass = {
         rowItem: 'row',
-        containerInput: 'col-12',
-        containerInputMiddle: 'col-6',
+        col1: 'col-1',
+        col11: 'col-11',
+        col12: 'col-12',
+        col6: 'col-6',
         containerField: 'd-flex flex-column justify-content-around align-items-center',
         sizeForm: 'tamañodiv form-field',
         labelInput: 'has-events',
@@ -42,29 +29,88 @@ export class CardPaymentComponent implements OnInit {
         txtCenter: 'text-center',
         customBtn: 'custom-button',
         appearance: 'outline',
-        floatLabel: 'always'
+        floatLabel: 'always',
+        backgroundGreend: 'background-green',
+        p15: 'padding-15',
+
+        // Aling items
+        aCenter: 'align-items-center'
     }
+    fullNames: string | undefined;
+    fullLastNames: string | undefined;
 
     @Output() nextCoverageStep = new EventEmitter<any>();
+
     title = "Configura tu tarjeta de crédito o débito";
     message = "Introduce tu tarjeta de débito o crédito para que puedas pagar tus servicios VIVA cada mes.";
 
     constructor(
         private webstoreService: WebstoreService
     ) {
-        console.log("########environment")
-        console.log(environment)
+        console.log("*******getClientInformation")
+        console.log(this.webstoreService.getClientInformation())//SIRVE
+        this.microFrontParamIn.user = this.webstoreService.getClientInformation();
+        console.log('*****TOKEN');
+        console.log(this.webstoreService.getDataInSession('token'));
+        this.fullNames = this.getNames();
+        this.fullLastNames = this.getLastNames();
     }
 
     ngOnInit(): void {
-        console.log("*******getAuthenticateInformation")
-        console.log(this.webstoreService.getClientInformation())//SIRVE
-        this.microFrontParamIn.user = this.webstoreService.getClientInformation();
-        console.log(this.microFrontParamIn.user)
-        console.log("*******getAuthenticateInformation")
-        console.log(this.webstoreService.getAuthenticateInformation())
+        // console.log("*******getAuthenticateInformation")
+        // console.log(this.webstoreService.getAuthenticateInformation())
         console.log("*******getMovilListInformation")
         console.log(this.webstoreService.getMovilListInformation())
+        const orderId = this.webstoreService.getDataInSession('orderMainId')
+        console.log("*******orderId")
+        console.log(orderId)
+        const person = this.webstoreService.getClientInformation();
+        console.log("*******personId")
+        console.log(person.personId)
+        console.log("*******getPlanComposition")
+        const planComposition = this.webstoreService.getPlanComposition();
+        console.log(planComposition)
+        const urlTerms = 'https://www.viva.com.bo/';
+
+        this.microFrontParamIn = {
+            theme: "light-green",
+            orderType: "SALES",
+            orderId: orderId ? orderId : null,
+            channel: "LANDING",// LANDING | OMEGA3
+            entityType: "partyId",//cableado
+            entityId: person && person.personId ? person.personId : null,//cableado,
+            language: "es",
+            termsOfService: `{mode:'required',url:'${urlTerms}'}`, // mode: required | option
+            amount: "150.90",
+            currency: "BOB",
+            cart: "[{sellerId:'NT', sellerDesc:'Viva'}]",
+            recurring: "required", // required | option
+            fullNames: this.fullNames,
+            fullLastNames: this.fullLastNames,
+            uniqueId: null,
+            payAmountMode: "required" // optional/required > CUANDO EL CHANEL ES OMEGA 3 Y EL PAY AMOUNT ES REQUIRED SE REALIZARA EL ENVIO DE MONTO DE LO CONTRARIO ENVIAR 0 
+            //OMEGA3
+            //payAmountMode: optional/required 
+            //si es contra entrega no mostrar el monto
+        }
+    }
+
+    getNames() {
+        const data = this.webstoreService.getClientInformation();
+        let name = data.name;
+        if(data.middleName != null){
+            name = name + ' ' + data.middleName;
+        }
+        return name.trim();
+    }
+
+    getLastNames() {
+        const data = this.webstoreService.getClientInformation();
+        let lastName = data.lastName1;
+        if(data.lastName2 != null){
+            lastName = lastName + ' ' + data.lastName2;
+        }
+        return lastName.trim();
     }
 
     toSendString(data: any) {
