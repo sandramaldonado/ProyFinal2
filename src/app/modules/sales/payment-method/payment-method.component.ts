27c110cb0ca9756b3261d9adfa6e6af3a3efcb45
Diego Olguin <diego.olguin@salamancasolutions.com>
2022-08-17 12:14:01 -0400

    changes payment method

--- src/app/modules/sales/payment-method/payment-method.component.ts
+++ src/app/modules/sales/payment-method/payment-method.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { OrdersService } from '@app/services/orders.service';
 import { WebstoreService } from '@app/services/webstore/webstore.service';
 
 @Component({
@@ -9,26 +8,33 @@ import { WebstoreService } from '@app/services/webstore/webstore.service';
 })
 export class PaymentMethodComponent implements OnInit {
 
+  optionSelected: any;
   listOfOptions = [
-    {value:"cardPayment",design:"credit_card",style:"background-color: #5C349D; color: white; border: 1px solid #5C349D; border-radius:16px;",name:"Tarjeta débito / crédito",id:"1",checked:true},
-    {value:"uponDelivery",design:"local_atm",style:"background-color:#5C339D; color: white; border: 1px solid #5C339D; border-radius:16px;",name:"Pago a contra entrega",id:"2",checked:false}
-    ];
+    {value:'cardPayment',design:'credit_card',name:'Tarjeta débito / crédito'}
+  ];
 
   @Output() nextMovilListStep = new EventEmitter<any>();
+  flow: any;
 
 
   constructor(
-    private webstoreservice: WebstoreService,
-    private ordersService: OrdersService) {
-
+    private webstoreservice: WebstoreService) {
+      this.flow = this.webstoreservice.getStatusScoring();
+      if(this.flow==='EXPRESS'){
+        this.listOfOptions.push(
+          {value:'uponDelivery',design:'local_atm',name:'Pago a contra entrega'}
+        );
+      }
   }
 
   ngOnInit(): void {
     console.log('init PaymentMethodComponent');
+    this.optionSelected = 'cardPayment';
   }
 
   radioChange(event: any) {
     console.log(event);
+    this.webstoreservice.saveDataInSession('paymentMethod', event.value);
   }
 
 }
